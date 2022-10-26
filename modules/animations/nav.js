e5ac1fca006242c0982ef6a9bb4d5346f6938854
michael86 <michael8t6@gmail.com>
2022-10-26 21:09:30 +0100

    GSAPPED the nav drop down on mobile

--- modules/animations/nav.js
+++ modules/animations/nav.js
@@ -1,17 +1,33 @@
 const hamburger = document.getElementById("hamburger");
+const nav = document.querySelector("nav");
+const navItems = nav.querySelectorAll("li");
+const primaryColor = "#161635";
+
+const tl = gsap.timeline({ delay: 0, paused: true });
+
+tl.to(nav, {
+  opacity: 1,
+  width: "100%",
+  height: "fit-content",
+
+  backgroundColor: primaryColor,
+  rotation: 360,
+  top:
+    document.getElementById("nav-container").getBoundingClientRect().height - 1,
+  duration: 0.5,
+}).to(navItems, {
+  color: "#fff",
+  duration: 0.1,
+  marginBottom: "10px",
+  scale: 1.3,
+  stagger: 0.2,
+});
+
 let navTog = false;
 
 hamburger.addEventListener("click", (e) => {
   navTog = !navTog;
 
-  navTog && openNav();
-  !navTog && closeNav();
+  navTog && tl.play();
+  !navTog && tl.reverse();
 });
-
-const openNav = () => {
-  console.log("open");
-};
-
-const closeNav = () => {
-  console.log("close");
-};
