a3c82bb6f532eb68303fffee557e6d29466732f7
Richard Curtis <richardc@creekviewelectronics.co.uk>
2022-11-22 16:34:17 +0000

    numerous updates

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1669644760;s:7:"authKey";s:64:"#EnuINSL8*XZ>,@t4yLD<CWz~w8W?8)~A2)UN)4KKV3%tf<~F{A9d9YL,X99v}A(";s:7:"version";s:5:"1.0.5";}
+a:6:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1669644760;s:7:"authKey";s:64:"#EnuINSL8*XZ>,@t4yLD<CWz~w8W?8)~A2)UN)4KKV3%tf<~F{A9d9YL,X99v}A(";s:7:"version";s:5:"1.0.5";s:11:"wafDisabled";b:1;}
